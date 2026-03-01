824afd6cdfa5bb0490f3e9610e6634f2c311ca05
Mariya Buslaeva <buslaeva.mariya11@gmail.com>
2026-03-01 18:54:27 +0400

    fix

--- src/test/java/ru/netology/web/test/BaseTest.java
+++ src/test/java/ru/netology/web/test/BaseTest.java
@@ -1,28 +1,40 @@
 package ru.netology.web.test;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class BaseTest {
 
     @BeforeAll
     static void setupAll() {
         Configuration.browser = "chrome";
-        Configuration.headless = false;
+        Configuration.headless = Boolean.parseBoolean(System.getProperty("selenide.headless", "true"));
+        Configuration.browserSize = "1366x768";
 
         Configuration.browserCapabilities.setCapability(
                 "goog:chromeOptions",
-                new java.util.HashMap<String, Object>() {{
-                    put("args", java.util.Arrays.asList(
-                            "--disable-popup-blocking",
-                            "--disable-notifications"
-                    ));
-                    put("prefs", new java.util.HashMap<String, Object>() {{
-                        put("profile.password_manager_leak_detection", false);
-                        put("credentials_enable_service", false);
-                        put("profile.password_manager_enabled", false);
-                    }});
-                }}
+                java.util.Map.of(
+                        "args", java.util.List.of(
+                                "--no-sandbox",
+                                "--disable-dev-shm-usage",
+                                "--remote-allow-origins=*",
+                                "--disable-popup-blocking",
+                                "--disable-notifications"
+                        ),
+                        "prefs", java.util.Map.of(
+                                "profile.password_manager_leak_detection", false,
+                                "credentials_enable_service", false,
+                                "profile.password_manager_enabled", false
+                        )
+                )
         );
+    }
+
+    @AfterEach
+    void tearDown() {
+        closeWebDriver();
     }
 }
